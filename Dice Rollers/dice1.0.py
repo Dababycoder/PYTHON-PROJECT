c994131a8a671c63782813728ed2a6a4cc8d1d08
Samuel Molero <73666791+samupro25@users.noreply.github.com>
2022-10-03 19:28:50 +0200

    Update dice 1.0
    Implementation of multiple dice rolling, and displaying it in percentages for the user

--- Dice Rollers/dice1.0.py
+++ Dice Rollers/dice1.0.py
@@ -1,4 +1,6 @@
+#implementation of multiple dice rolls at the same time and showing what each dice rolled individually
 import random
+from collections import Counter
 
 #Welcoming the user to the game
 print("Hello and welcome to the dice roller!")
@@ -11,9 +13,26 @@ while True:
    
    #if statement to determine the number of dice faces, and to "roll the dice" hypothetically.
    if wants_to_play == "y":
-      dice_face = int(input("Select the number of faces you want your dice to have. WARNING: if you don't select a whole number, the dice roller won't work. "))
-      generator = random.randint(0, dice_face)
-      print("The number rolled by your", dice_face, "face dice was:", generator)
+      die_face = int(input("Select the number of faces you want your dice to have. WARNING: if you don't select a whole number, the dice roller won't work. "))
+      die_amount = int(input("Select how many dices of the given faces you want to roll: "))
+
+      #generating dice roll number with the given input
+      die_rolls = []
+      for i in range(0, die_amount):
+         die_rolls.append(random.randint(1,die_face))
+
+      #counting how many of each element in the list are they
+      num = Counter(die_rolls)
+      percentages = sum(num.values())
+      total = round(percentages, 1)
+
+
+      #printing final message output to the user
+      print("Your dice percentages where: ")
+      for face, count in num.most_common():
+         print(f"{face}: {count} ({count/total:.2%})")
+
+
    
    #When the user stops wanting to play, saying them goodbye
    else:
